2d3028aa735bf98403523cbbf8aa8fd10e35291b
FranciscoPerczyk <fperczyk@fi.uba.ar>
2022-05-28 18:59:12 -0300

    Mejoro el punto 5 que al final iba en el punto 10

--- functions.R
+++ functions.R
@@ -23,3 +23,19 @@ adjust_data = function(data,name,comparisson,criteria){
   missing_values= which(comparisson(data[name], criteria))
   return(data[-missing_values,])
 }
+
+stepwise = function(datainfunction){
+  attach(datainfunction)
+  vars = cbind(Glucose, Pregnancies, BloodPressure, SkinThickness, Insulin, DiabetesPedigreeFunction, Age)
+  exhaustive<-regsubsets(datainfunction$BMI~vars,data = datainfunction, method = "exhaustive")
+  
+  par(mfrow=c(2,2))
+  plot(summary(exhaustive)$rss,pch=20,xlab="Modelo", ylab= "RSS")
+  plot(summary(exhaustive)$rsq,pch=20,xlab="Modelo", ylab= "R^2")
+  plot(summary(exhaustive)$adjr2,pch=20,xlab="Modelo", ylab= "R^2 aj")
+  plot(summary(exhaustive)$cp,pch=20,xlab="Modelo", ylab= "CP")
+  abline(0,1)
+  par(mfrow=c(1,1))
+  
+  return(exhaustive)
+}
